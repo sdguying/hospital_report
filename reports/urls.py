9029df3b722b1319051be09c6070923891610766
liguannan <lgn@liguannan.com>
2019-08-15 23:23:55 +0800

    增加删除报告的功能，修改了一些bug

--- reports/urls.py
+++ reports/urls.py
@@ -6,7 +6,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reports.html', views.reports_index, name='reports_index'),
+    path('reports_index.html', views.reports_index, name='reports_index'),
     path('<int:report_id>/show_report.html', views.show_report, name='show_report'),
     path('edit/<int:report_id>/edit_report_info.html', views.edit_report_info, name='edit_report_info'),
+    path('del/<int:report_id>/del_report.html', views.del_report, name='del_report'),
 ]
