63c6af93273407f797241f5aff30405c05fe5564
liguannan <lgn@liguannan.com>
2019-08-22 21:55:28 +0800

    初步实现了点击具体的检查项目展现现有报告的可视化图形

---
+++ reports/templatetags/reports_func.py
@@ -0,0 +1,11 @@
+from django import template
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_img_url(img):
+    """图片地址"""
+    image_path = '/images/reports/' + str(img.id) + '.png'
+    return static(image_path)
