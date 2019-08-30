704475b498e07b99df07c0aee42e2a5b24850813
liguannan <lgn@liguannan.com>
2019-08-30 21:04:56 +0800

    select实现下拉框显示用户的科室，但是如何回传参数使保存entry的时候有效

--- reports/forms.py
+++ reports/forms.py
@@ -32,13 +32,13 @@ class ReportForm(forms.ModelForm):
         }
 
 
-# class CategoryForm(forms.Form):
+# class CategoryForm_select(forms.Form):
 #     """为category模型作一个动态的下拉框"""
 #     category = forms.ChoiceField(label='科室')
 #
 #     def __init__(self, *args, **kwargs):
 #         """"""
-#         super(CategoryForm, self).__init__(*args, **kwargs)
+#         super(CategoryForm_select, self).__init__(*args, **kwargs)
 #         self.fields['category'].choices = (( x.name ) for x in Category.objects.all())
 
 
